481b82bd0e55d03e660d8fa352587364a95b8590
Menna Mohamed <mennamohammed178@gmail.com>
2025-03-23 04:39:55 +0200

    fix port issue

--- app.py
+++ app.py
@@ -124,5 +124,5 @@ def serve_logo():
     return send_from_directory('templates', 'logo.png')
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True, allow_unsafe_werkzeug=True)
+    port = int(os.environ.get("PORT", 8080))
+    socketio.run(app, host="0.0.0.0", port=port, debug=True, allow_unsafe_werkzeug=True)
